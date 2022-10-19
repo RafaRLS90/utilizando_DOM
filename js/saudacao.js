b2e646fb9af633a4312596aced126b56f1763146
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2022-10-19 00:01:28 -0300

    adicionando ação ao botão enviar

--- js/saudacao.js
+++ js/saudacao.js
@@ -30,3 +30,11 @@
 
     }
 })()
+
+    const txtEmail = document.getElementById("txtEmail")
+    const msgFeedback = document.getElementById("newsletterFeedback")
+
+    function cadastrarEmail() {
+        let email = txtEmail.value //email = "" elemento vazio
+        msgFeedback.innerHTML = `O email ${email} foi cadastrado com sucesso!`
+    }
